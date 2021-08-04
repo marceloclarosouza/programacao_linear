31b87281b2e55ab448df6d76d121b2b7ac4d3e48
marceloclarosouza <marcelo.claro.zouza@gmail.com>
2021-08-04 19:30:49 -0300

    exemplo2

--- aula28_02_2021.py
+++ aula28_02_2021.py
@@ -19,7 +19,7 @@ m = len(d) # numero de materias primas
 
 
 begin("ligas") # particularidade do pacote pymathprog p iniciar o modelo ligas
-
+verbose("True")
 # variaveis de decisao
 
 x = var("x", n) # cria n variaveis continuas (x0, x1, ... xn-1)
@@ -29,7 +29,7 @@ maximize(sum(r[i]*x[i] for i in range(n)))
 
 # restricoes
 for j in range(m):
-sum(p[j][i]*x[i] for i in range(n)) <= d[j]
+     sum(p[j][i]*x[i] for i in range(n)) <= d[j]
 
 # resolvendo o modelo
 solve()
@@ -40,6 +40,50 @@ print(f"Valor otimo: {vobj()}")
 print("Solucao otima")
 
 for i in range(n):
-print("x[{}] = {:.3f}". format(i+1, x[i].primal))
+     print("x[{}] = {:.3f}". format(i+1, x[i].primal))
+
+end()
+
+# ======================================================================
+# Exemplo 2
+"""
+Suponha que no exemplo anterior as ligas produzidas são vendidas em
+barras de 1 tonelada. Elabore o modelo de programação matemática que maximize a
+receita bruta na venda das barras.
+"""
+
+# Dados de entrada
+r = [3, 2] # receita de cada liga
+d = [3, 1, 3] #estoque de cada materia prima
+p = [[0.5, 0.3],
+     [0.1, 0.2],
+     [0.4, 0.5]] # percentuais das materias primas nas ligas
+n = len(r) # numero de ligas
+m = len(d) # numero de materias primas
+
+
+begin("ligas") # particularidade do pacote pymathprog p iniciar o modelo ligas
+
+# variaveis de decisao
+
+x = var("x", n, int) # cria n variaveis continuas (x0, x1, ... xn-1) (int é do exemplo2)
+
+# funcao objetivo
+maximize(sum(r[i]*x[i] for i in range(n)))
+
+# restricoes
+for j in range(m):
+     sum(p[j][i]*x[i] for i in range(n)) <= d[j]
+
+# resolvendo o modelo
+solve()
+
+# apresentando a solucao
+print(f"Valor otimo: {vobj()}")
+
+print("Solucao otima")
+
+for i in range(n):
+     print("x[{}] = {:.3f}". format(i+1, x[i].primal))
 
 end()
