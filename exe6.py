dd5616afd3821566140fe69c96b50df1ab1ac38c
marceloclarosouza <marcelo.claro.zouza@gmail.com>
2021-08-25 20:02:45 -0300

    exe6

---
+++ exe6.py
@@ -0,0 +1,42 @@
+# Exercício 6
+from pymprog import*
+
+# Dados de entrada
+T = [54, 83, 15, 71, 77, 36, 53, 38, 27, 87, 76, 91, 14, 29, 12, 77, 32, 87, 
+     68, 94, 79, 3, 11, 99, 56, 70, 99, 60, 5, 56, 3, 61, 73, 75, 47, 14, 21,
+     86, 5, 77, 16, 89] # tempos de realização das tarefas
+
+n = 6 # número de operadores
+m = len(T) # número de tarefas
+
+begin("tarefas") # inicia um modelo
+# variáveis de decisão
+A = iprod(range(n), range(m)) # indices da variável x
+x = var('x', A, bool) # cria variável x
+z = var('z') # cria variável para o makespan (variavel continua)
+
+
+# funcao objetivo
+minimize(z)
+
+# restricoes
+for j in range(m):
+  sum(x[i,j] for i in range(n)) == 1 # alocacao de tarefas
+
+for i in range(n):
+  sum(T[j]*x[i,j] for j in range(m)) <= z # definicao do mekspan
+
+
+# resolvendo
+solve()
+print("Valor ótimo = {}".format(vobj()))
+
+for i in range(n):
+  print("Operador {}: ".format(i+1), end=" ")
+  for j in range(m):
+    if round(x[i,j].primal) == 1:
+      print("{} ({})   ".format(j+1,T[j]), end=" ")
+  print()
+  print()
+
+end()
